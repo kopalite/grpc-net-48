95e0db1c7ce410ca787e9ac66bb321d9727283ad
Ivan Kopcanski <ivan.kopcanski@hotmail.com>
2021-05-14 16:22:12 +0200

    initial commit

---
+++ GreetClient/TestGrpc.Client.Lib/GreetClientTool.cs
@@ -0,0 +1,38 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using TestGrpc;
+using static TestGrpc.Client.Lib.Greeter;
+
+namespace TestGrpc.Client.Lib
+{
+    public class GreetClientTool
+    {
+        public async Task GreetPlainAsync()
+        {
+            var channel = new Channel("localhost", 5011, ChannelCredentials.Insecure);
+            var client = new GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+        }
+
+        public async Task GreetSecureAsync()
+        {
+            var credentials = new SslCredentials(GetCertPem());
+            channel = new Channel("localhost", 5012, credentials);
+            client = new GreeterClient(channel);
+            reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+        }
+    }
+
+    
+}
---
+++ GreetClient/TestGrpc.Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using TestGrpc;
+using static TestGrpc.Client.Greeter;
+
+namespace TestGrpc.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+           
+        }
+
+        
+    }
+}
